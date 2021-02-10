b61a69e815797735e934a8cca8f9fdba78139c2e
Elijah Okello <elijahokello90@gmail.com>
2021-02-10 16:37:27 +0300

    icons

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, DetailView
 
-
+app_name = "blog"
 #url patterns for the blog application
 urlpatterns = [
 	path("",HomeView.as_view(),name="blog_home"),
+	path("detail/<int:pk>/",DetailView.as_view(),name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,15 @@ class HomeView(generic.ListView):
 	template_name = 'blog/home.html'
 	model = Post
 	context_object_name = 'posts'
-
+	
     # def get_context_data(self, **kwargs):
     #     # Call the base implementation first to get a context
     #     context = super().get_context_data(**kwargs)
     #     # Add in a QuerySet of all the books
-    #     context['title'] = Book.objects.all()
+    #     context['title'] = "&C"
     #     return context
+
+class DetailView(generic.DetailView):
+    template_name = 'blog/detail.html'
+    model = Post
+    context_object_name = 'post'
