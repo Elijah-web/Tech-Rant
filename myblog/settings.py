389ca698a599e84ca3224a6377cfc8febaf7ff96
Elijah Okello <58866119+Elijah-web@users.noreply.github.com>
2021-02-15 13:30:59 +0300

    Debug Value

--- myblog/settings.py
+++ myblog/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See htts://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'zy@5t*m@g#_zeg9@d6g92^4eb%-!#@4y!2%=poq0s)))*-idf+'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
